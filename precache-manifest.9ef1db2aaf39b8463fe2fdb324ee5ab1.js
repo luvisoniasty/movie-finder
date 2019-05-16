3f5dca473bdeb4c39654e2910d0a7775f08c5e56
luvisoniasty <luvisonx@gmail.com>
2019-05-16 15:09:13 +0200

    Updates

---
+++ precache-manifest.9ef1db2aaf39b8463fe2fdb324ee5ab1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0ccb96848eac9dac43b4f8fcf2cf42",
+    "url": "/movie-finder/index.html"
+  },
+  {
+    "revision": "d1ccbda107355f845266",
+    "url": "/movie-finder/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "d1ccbda107355f845266",
+    "url": "/movie-finder/static/js/2.cc2c156f.chunk.js"
+  },
+  {
+    "revision": "0a7bb55c62d61b1d1658",
+    "url": "/movie-finder/static/js/main.31612feb.chunk.js"
+  },
+  {
+    "revision": "a667e2b1cde792e0d9a3",
+    "url": "/movie-finder/static/js/runtime~main.e473438c.js"
+  }
+]);
